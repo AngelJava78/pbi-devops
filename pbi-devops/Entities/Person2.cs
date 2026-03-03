c554f9668a65f97165ec44de132a36b86d55412a
E0001276 <angel.javier1@profuturo.com.mx>
2026-03-03 16:56:30 -0600

    First commit

---
+++ pbi-devops/Entities/Person2.cs
@@ -0,0 +1,15 @@
+namespace pbi_devops.Entities;
+// English
+public class Person2
+{
+	public long Id { get; set; }
+	public string Name { get; set; }
+	public string Lastname { get; set; }
+	public DateTime BirthDate { get; set; }
+	public string Email { get; set; }
+	public int Age { get; set; }
+	public string Country { get; set; }
+	public string City { get; set; }
+	public string Street { get; set; }
+	public string Number { get; set; }
+}
