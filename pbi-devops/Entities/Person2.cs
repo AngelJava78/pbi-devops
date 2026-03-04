f0da5003aeee62dcd07296810c55fd87f24eb6b2
AngelJava78 <AngelJava78@users.noreply.github.com>
2026-03-04 00:00:36 +0000

    UP: Remplazo de variables del JSON config(QA), rama release

--- pbi-devops/Entities/Person2.cs
+++ pbi-devops/Entities/Person2.cs
@@ -1,15 +1,15 @@
 namespace pbi_devops.Entities;
-// Español
+// Français
 public class Person2
 {
 	public long Id { get; set; }
-	public string Nombre { get; set; }
-	public string Apellidos { get; set; }
-	public DateTime FechaNacimiento { get; set; }
-	public string Correo { get; set; }
-	public int Edad { get; set; }
-	public string Pais { get; set; }
-	public string Ciudad { get; set; }
-	public string Calle { get; set; }
+	public string Prenom { get; set; }
+	public string NomDeFamille { get; set; }
+	public DateTime DateDeNaissance { get; set; }
+	public string Email { get; set; }
+	public int Age { get; set; }
+	public string Pays { get; set; }
+	public string Ville { get; set; }
+	public string Rue { get; set; }
 	public string Numero { get; set; }
 }
